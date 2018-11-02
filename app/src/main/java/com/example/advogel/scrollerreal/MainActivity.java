c8892236440d77762aedcec0b3ea4cdf3ae51100
advogel <34170224+advog@users.noreply.github.com>
2018-11-02 10:30:44 -0400

    SHine

--- app/src/main/java/com/example/advogel/scrollerreal/MainActivity.java
+++ app/src/main/java/com/example/advogel/scrollerreal/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -14,12 +16,13 @@ public class MainActivity extends AppCompatActivity {
     Button b;
 
     Button q1;
-    Button q2;
+    EditText q2;
     Button q3;
     Button q4;
     Button q5;
     Button q6;
     Button q7;
+    Button q8;
 
     Snackbar snack;
     @Override
@@ -28,12 +31,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         q1 = (RadioButton) findViewById(R.id.radioButton11); //1
-        q2 = (RadioButton) findViewById(R.id.radioButton22); //2
+        q2 = (EditText) findViewById(R.id.radioButton22); //2
         q3 = (RadioButton) findViewById(R.id.radioButton31); //1
         q4 = (RadioButton) findViewById(R.id.radioButton41); //1
         q5 = (RadioButton) findViewById(R.id.radioButton52); //2
         q6 = (RadioButton) findViewById(R.id.radioButton61); //1
-        q7 = (RadioButton) findViewById(R.id.radioButton71); //1
+        q7 = (CheckBox) findViewById(R.id.radioButton71); //1
+        q8 = (CheckBox) findViewById(R.id.radioButton72); //1
         snack = Snackbar.make(findViewById(R.id.parent),"",Snackbar.LENGTH_LONG);
     }
 
@@ -42,17 +46,21 @@ public class MainActivity extends AppCompatActivity {
 
         if(q1.isSelected())
             correct++;
-        if(q2.isSelected())
+
+        if(q2.getText().equals("shamalama"))
             correct++;
+
         if(q3.isSelected())
             correct++;
         if(q4.isSelected())
             correct++;
         if(q5.isSelected())
             correct++;
+
         if(q6.isSelected())
             correct++;
-        if(q7.isSelected())
+
+        if(q7.isSelected() && q8.isSelected())
             correct++;
 
         snack.setText(correct + "/7");
